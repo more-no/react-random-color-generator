584ba04960a48d9fb431cbce6cea9f2df366a3df
Moreno Dore <moreno.dore@tuta.io>
2023-09-11 21:35:14 +0200

    Change message

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
           height: '200px',
         }}
       >
-        Generated Color: <{newColor}>
+        Generated Color: {newColor}
       </div>
     </div>
   );
