aa81d69dd8d5a6ca4e085727251aa0da494596a9
Moreno Dore <moreno.dore@tuta.io>
2023-09-11 17:38:55 +0200

    Complete initial tasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
-import './App.css';
+import randomColor from 'randomcolor';
+import { GetColorName } from 'hex-color-to-color-name';
 
 export default function App() {
-  return <div></div>;
+  const newColor = randomColor();
+  const colorName = GetColorName(newColor);
+
+  return (
+    <div>
+      <h1> React Random Color Generator</h1>
+
+      <button onClick={newColor}>Generate</button>
+
+      <br />
+      <br />
+      <div
+        style={{
+          color: 'black',
+          backgroundColor: newColor,
+          width: '400px',
+          height: '200px',
+        }}
+      >
+        <p style={{ textAlign: 'center' }}>
+          <br />
+          Generated Color: <br /> <br />
+          {colorName} <br /> <br />
+          {newColor}
+        </p>
+      </div>
+    </div>
+  );
 }
